15ca8b8bc879f535e8605266bb1ce9de83a2b214
Nigel Teo <e0694450@u.nus.edu>
2025-03-30 00:05:46 +0800

    Add closing of dropdown if outside is clicked

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 const menuButton = document.getElementById("menuButton");
 const dropdownMenu = document.getElementById("dropdownMenu");
 
-menuButton.addEventListener("click", () => {
+menuButton.addEventListener("click", (e) => {
+  e.stopPropagation(); // prevent it from triggering the outside click
   menuButton.classList.toggle("active");
   dropdownMenu.classList.toggle("active");
+});
+
+// Close the menu when clicking outside
+document.addEventListener("click", (e) => {
+  if (!menuButton.contains(e.target) && !dropdownMenu.contains(e.target)) {
+    menuButton.classList.remove("active");
+    dropdownMenu.classList.remove("active");
+  }
 });
