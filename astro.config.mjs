05980b553674c8a862e5ae67fd4e64479ad6347a
Nigel Teo <nigelteosw@gmail.com>
2025-07-30 16:35:13 +0800

    update setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	base: '/',
-  integrations: [react(), tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [react()],
 });
