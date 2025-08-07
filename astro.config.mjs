e707cd4a7b04270fde66b3370ffb448c7a95bc1c
Nigel Teo <e0694450@u.nus.edu>
2025-08-07 16:06:19 +0800

    update config for github deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://nusseafarers.github.io/',
   vite: {
     plugins: [tailwindcss()],
   },
